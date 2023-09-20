690e0cfe0734029c78075140767fdecc884fef9a
PedroPortow <pedrolportow@gmail.com>
2023-09-20 15:12:22 -0300

    feat: removed books unused table

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_08_013449) do
-  create_table "books", force: :cascade do |t|
-    t.string "name"
-    t.string "author"
-    t.integer "pages"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+ActiveRecord::Schema[7.0].define(version: 2023_09_20_181130) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.string "description"
