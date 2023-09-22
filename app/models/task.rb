7664d4990c2b11d0764d092cf6a631fdbb3ac971
PedroPortow <pedrolportow@gmail.com>
2023-09-22 00:08:20 -0300

    lil changes

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,5 @@ class Task < ActiveRecord::Base
   belongs_to :status_mapping, foreign_key: 'status', primary_key: 'status'
   belongs_to :priority_mapping, foreign_key: 'priority', primary_key: 'priority'
 
+  validates :title, presence: true, length: { minimum: 3, maximum: 100}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # }
 
   resources :tasks
-  resources :books
+  resources :books, only: [:show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
